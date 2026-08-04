d84b9018ac8e26efc4ad1ce6e5851b782ce2e7de
Gregori Evangelista Jimenez <gregorievangelista43@gmail.com>
2026-08-04 12:22:12 -0400

    Remove comment from IStagingService interface
    Removed comment explaining the purpose of the interface.

--- AnalisisVentas.Data/Interfaces/IStagingService.cs
+++ AnalisisVentas.Data/Interfaces/IStagingService.cs
@@ -1,7 +1,6 @@
 namespace AnalisisVentas.Data.Interfaces;
 
 // Contrato para persistir los datos extraídos en archivos temporales (staging),
-// cumpliendo el requisito de la fase de extracción del ETL.
 public interface IStagingService
 {
     Task WriteAsync<T>(string name, IEnumerable<T> records, CancellationToken cancellationToken = default);
